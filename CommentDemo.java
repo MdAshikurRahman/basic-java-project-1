da300cd810b3b8ab66ed23b4c1836897a43cece2
MdAshikurRahman <mdashikurrahman.online@gmail.com>
2019-11-02 22:01:16 +0600

    new JavaProject1.java file create

---
+++ CommentDemo.java
@@ -0,0 +1,19 @@
+
+package javaproject1;
+
+public class CommentDemo {
+    
+    public static void main(String[] args){
+        
+        //displaying my name
+        
+/*
+        Date
+        Name
+        */
+
+        System.out.println("Md Ashikur Rahman");
+        
+        System.out.println("Md Ashikur Rahman");
+    }
+}
---
+++ JavaProject1.java
@@ -0,0 +1,17 @@
+
+package javaproject1;
+
+public class JavaProject1 {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Md Ashikur Rahman\n Nayon");
+        System.out.println("01717012183");
+        System.out.println("BSc in EEE");
+        System.out.println("1");
+        System.out.println("1\t 2");
+        System.out.println("3\t 4");
+        System.out.println(" \"Java Programming\"");
+      
+    }
+}
